df6ff61f97b0deeb58c82c89995dc74635240ef2
MirzaAyon <ayonmirza98@gmail.com>
2022-05-15 21:40:53 +0600

    while loop added

---
+++ while-loop3.js
@@ -0,0 +1,12 @@
+var roastGiven = 0;
+
+while (roastGiven < 7) {
+
+    console.log('Roast den');//etar pasher red light e click korsi
+    roastGiven++;
+    console.log(roastGiven);
+}
+//break point diye debug korbo 
+//stepover or F10 use korbo
+//shesh hoye gele r dhukbe na 
+//tokhn run and debug code execute kora shesh hoye jabe 
